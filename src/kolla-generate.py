25da04f33c95ae0b9b6be3a30b69a72d35fdf673
Christian Berendt <berendt@betacloud-solutions.de>
2017-08-02 11:05:55 +0200

    Use releases instead of revisions

--- src/kolla-generate.py
+++ src/kolla-generate.py
@@ -8,10 +8,11 @@ import os
 import jinja2
 import json
 import requests
+import semver
 import yaml
 
 today = datetime.date.today()
-BUILD = os.environ.get("BUILD", today.strftime("%Y%m%d"))
+OSISM_VERSION = os.environ.get("OSISM_VERSION", today.strftime("%Y%m%d"))
 
 DOCKER_REGISTRY = os.environ.get('DOCKER_REGISTRY', 'hub.docker.com')
 DOCKER_NAMESPACE = os.environ.get('DOCKER_NAMESPACE', 'osism')
@@ -19,34 +20,7 @@ DOCKER_NAMESPACE = os.environ.get('DOCKER_NAMESPACE', 'osism')
 TEMPLATE_PROJECT = "kolla.tmpl"
 DESTINATION = "images"
 
-
-def get_tags_of_image(image):
-    response = requests.get("https://%s/v2/repositories/%s/%s/tags/" % (DOCKER_REGISTRY, DOCKER_NAMESPACE, image))
-
-    result = []
-    for tag in response.json().get('results', []):
-        result.append(tag['name'])
-    return result
-
-
-def get_last_revision_of_project(project, tag):
-    with open("tmp/tmp-%s.lst" % project, 'r') as fp:
-        image = fp.readline().strip().replace("_", "-")
-
-    if project == "openvswitch_db":
-        image = "openvswitch-db-server"
-
-    last_revision = 0
-    for itag in get_tags_of_image(image):
-         if str(itag).startswith(str(tag)) and "-" in str(itag) and not str(itag).endswith("latest"):
-             revision = itag.split("-")[1]
-             if revision > last_revision:
-                 last_revision = revision
-
-    return last_revision
-
-
-with open("versions/current.yml", "rb") as fp:
+with open("versions/%s.yml" % OSISM_VERSION, "rb") as fp:
     versions = yaml.load(fp)
 
 # http://stackoverflow.com/questions/38987/how-to-merge-two-python-dictionaries-in-a-single-expression
@@ -61,17 +35,19 @@ environment = jinja2.Environment(loader=loader)
 template_project = environment.get_template(TEMPLATE_PROJECT)
 
 projects = []
-revisions = {}
 for lstfile in glob.iglob("tmp/tmp-*.lst"):
     project_name = (lstfile[:-4])[8:]
     if project_name in all_projects:
-        revisions[project_name] = get_last_revision_of_project(project_name, all_projects[project_name])
         projects.append(project_name)
 
 all_projects = collections.OrderedDict(sorted(all_projects.items()))
+for project in all_projects:
+    version = semver.parse_version_info(str(all_projects[project]))
+    all_projects[project] = "%s.%s" % (version.major, version.minor)
 
-with open(os.path.join(DESTINATION, "kolla-%s.yml" % BUILD), "w") as fp:
+with open(os.path.join(DESTINATION, "kolla-%s.yml" % OSISM_VERSION), "w") as fp:
     fp.write("---\n")
+    fp.write("osism_version: %s\n\n" % OSISM_VERSION)
 
 for lstfile in glob.iglob("tmp/tmp-*.lst"):
     with open(lstfile) as fp:
@@ -84,9 +60,8 @@ for lstfile in glob.iglob("tmp/tmp-*.lst"):
             "project_name": project_name,
             "project_version": all_projects[project_name],
             "project_images": images,
-            "revisions": revisions,
-            "build": BUILD
+            "osism_version": OSISM_VERSION
         })
 
-        with open(os.path.join(DESTINATION, "kolla-%s.yml" % BUILD), "a+") as fp:
+        with open(os.path.join(DESTINATION, "kolla-%s.yml" % OSISM_VERSION), "a+") as fp:
             fp.write(result)
